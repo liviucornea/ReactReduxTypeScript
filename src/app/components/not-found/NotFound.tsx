2fd24cd03307481ff9b4cdcd9c3ebfe8f38a1aa0
Liviu Cornea <liviumcornea@gmail.com>
2020-10-24 14:13:14 -0400

    Script to run with backend API

--- src/app/components/not-found/NotFound.tsx
+++ src/app/components/not-found/NotFound.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NotFound() {
     return (<div>
-        <h2>Resorce not found</h2>
+        <h2>Resorce not found.  Please try different route!</h2>
     </div>
     )
 } 
